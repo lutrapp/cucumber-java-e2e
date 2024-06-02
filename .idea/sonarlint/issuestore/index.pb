a00a22e658b8ed0323071b16c339023b1f9bdede
lutrapp <lutrapp@gmail.com>
2024-06-02 00:23:29 -0300

    test runner and a features examples

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,11 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 S
 #src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+9
+	notes.txt,8\3\83348d283c1ba0b3b4d55fdc04c39a165b933125
+\
+,src/test/java/StepDefinition/LoginSteps.java,6\5\6571301296a1bc6dd4824cde50c4583f344d69ac
+]
+-src/test/java/StepDefinitions/LoginSteps.java,4\b\4b50ec51e4e7d7c78e9b30547caa2bc3d233196d
+]
+-src/test/java/StepDefinitions/TestRunner.java,5\d\5da2d9743ad5da596c719c1a81a789da3eace5ca
---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,18 @@
+package StepDefinitions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/resources/Features",
+        glue ={"StepDefinitions"},
+        monochrome = true,
+        plugin = {"pretty",
+                "html:target/cucumber/report.html",
+                "json:target/cucumber/report.json"},
+//        tags = "@smoketest" // to run specific scenarios with this annotation on Features
+)
+public class TestRunner {
+}
